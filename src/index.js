b652cdd6f75c2bb793325b2d6089066100f0f889
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-01-06 22:20:18 +0900

    [Chore] 개발환경 셋업 (#7)
    ## 배경 설명
    - 개발 과정에서 작성한 페이지를 서빙하기 위한 스크립트 등을 작성했습니다.
    
    ## 작업한 내용
    - 페이지 서빙용 스크립트 작성
    - `index.js` 호스팅 테스트

---
+++ src/index.js
@@ -0,0 +1,11 @@
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
