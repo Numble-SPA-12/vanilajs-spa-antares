47b5011a7d59ddc74f8bcb5e66124376b59bf483
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-01-14 02:52:56 +0900

    [Chore] Home 페이지 코드 리팩토링

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,26 +3,27 @@
 import { getPosts } from "apis/PostsAPI";
 import { PostPreview } from "components";
 import router from "router";
+import parseElementFromString from "utils/parseElementFromString";
 
 const Home = async () => {
-  const $main = document.createElement("div");
-
-  $main.classList.add("flex", "flex-col", "justify-start", "min-h-screen");
-
-  $main.innerHTML = `
-    <app-header type="main"></app-header>
-    <main class="flex-1 px-4 pt-4 pb-8 bg-gray-50">
-      <button id="newyear-post__newpost-link" class="w-full py-3 rounded-lg bg-black text-white font-semibold text-lg mb-4">New Post</button>
-      <ul id="newyear-post__list" class="flex flex-col gap-4">
-        ${PostPreview.Loading().outerHTML}
-        ${PostPreview.Loading().outerHTML}
-        ${PostPreview.Loading().outerHTML}
-        ${PostPreview.Loading().outerHTML}
-      </ul>
-    </main>
+  const pageString = `
+    <div class="flex flex-col justify-start min-h-screen" > 
+      <app-header type="main"></app-header>
+      <main class="flex-1 px-4 pt-4 pb-8 bg-gray-50">
+        <button id="newyear-post__newpost-link" class="w-full py-3 rounded-lg bg-black text-white font-semibold text-lg mb-4">New Post</button>
+        <ul id="newyear-post__list" class="flex flex-col gap-4">
+          ${PostPreview.Loading()}
+          ${PostPreview.Loading()}
+          ${PostPreview.Loading()}
+          ${PostPreview.Loading()}
+        </ul>
+      </main>
+    </div> 
   `;
 
-  $main.querySelector("#newyear-post__list").addEventListener("click", (e) => {
+  const $page = parseElementFromString(pageString);
+
+  $page.querySelector("#newyear-post__list").addEventListener("click", (e) => {
     const $target = e.target.closest("article");
     if ($target) {
       const id = $target.dataset.postId;
@@ -30,24 +31,24 @@ const Home = async () => {
     }
   });
 
-  $main
+  $page
     .querySelector("#newyear-post__newpost-link")
     .addEventListener("click", (e) => {
       router.push("/posts/new");
     });
 
   (async () => {
-    const $list = $main.querySelector("#newyear-post__list");
+    const $list = $page.querySelector("#newyear-post__list");
     const result = await getPosts();
     if (result.code === 200) {
       console.log(result.data);
       $list.innerHTML = result.data.posts
-        .map((post) => PostPreview(post).outerHTML)
+        .map((post) => PostPreview(post))
         .join("");
     }
   })();
 
-  return $main;
+  return $page;
 };
 
 export default Home;
