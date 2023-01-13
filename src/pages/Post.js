202cbba6824e926ac6c9625b5206e8f2cf696e8d
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-01-13 18:14:50 +0900

    edit & delete 버튼 추가

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -15,7 +15,7 @@ const Post = () => {
           <section id="post__image" class="w-full">
             <img src="https://picsum.photos/id/1/360/265" class="h-full w-full object-cover aspect-[360/265]" />
           </section>
-          <section id="post__content" class="px-4 pt-4 pb-8 flex flex-1 flex-col gap-4">
+          <section id="post__content" class="px-4 pt-4 flex flex-1 flex-col gap-4">
             <div id="post__content__meta flex flex-col gap-2" >
               <h2 id="post__content__title" class="text-2xl font-semibold">제목</h2>
               <time datetime="2023-01-08" class="text-xs text-gray-500" >2023-01-08</time>
@@ -28,6 +28,10 @@ const Post = () => {
               lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
               lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
             </p>
+          </section>
+          <section id="post__actions" class="flex gap-2 justify-end w-full px-4 py-4" >
+            <button class="w-14 py-0.5 rounded-sm text-sm hover:bg-gray-200 text-gray-600">Edit</button>
+            <button class="w-14 py-0.5 rounded-sm text-sm hover:bg-gray-200 text-gray-600">Delete</button>
           </section>
         </article>
         <aside id="post__comments" class="p-4 flex flex-col gap-4">
