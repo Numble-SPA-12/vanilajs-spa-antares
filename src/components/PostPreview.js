94996cd62e94a2c7f5e72a28f70b973af1331940
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-01-14 02:35:36 +0900

    [Feat] 홈 페이지에 대해서 Data Fetching 및 XSS 방지 코드 구현 (#25)
    ## 배경 설명
    - 홈 페이지에 서버에서 받아온 데이터를 연결하는 작업을 진행했습니다.
    - 크로스 사이트 스크립팅 방지용 escape 코드를 작성했습니다.
    
    ## 작업한 내용
    - `String.prototype.escape()`를 이용해 XSS 방지가 가능하도록 했습니다.
    - `PostPreview`의 input Props를 서버 측 데이터 형태와 비슷하도록 변경하고, CSS 수정이 있었습니다.
    - 서버에서 데이터를 불러오는 동안 로딩 처리가 되도록 수정했습니다.
    - 서버에서 데이터를 받아와 포스트 리스트를 업데이트 하는 코드를 작성했습니다.

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,14 +1,21 @@
 import parseElementFromString from "utils/parseElementFromString";
 
-const PostPreview = ({ id, title = "제목", content = "내용", imgSrc = "" }) => {
+const PostPreview = ({
+  postId,
+  title = "제목",
+  content = "내용",
+  image = "",
+  createdAt = "",
+  updatedAt = "",
+}) => {
   const componentString = `
-    <article id="newyear-post-${id}" data-post-id="${id}" class="flex rounded-md border bg-white border-gray-300 overflow-hidden cursor-pointer">
-      <section class="shrink-0 w-30 h-30">
-        <img src="${imgSrc}" loading="lazy" />
+    <article id="newyear-post-${postId}" data-post-id="${postId}" class="flex rounded-md border bg-white border-gray-300 overflow-hidden cursor-pointer">
+      <section class="shrink-0 w-[100px] h-[100px]">
+        <img src="${image.escape()}" class="h-full w-full object-cover aspect-[100/100]" loading="lazy" />
       </section>
       <section class="flex flex-1 flex-col justify-between px-3 py-4 gap-2 overflow-hidden">
-        <h2 class="text-lg font-bold">${title}</h2>
-        <p class="text-sm text-gray-500 max-w-full overflow-hidden text-ellipsis whitespace-nowrap">${content}</p>
+        <h2 class="text-lg font-bold max-w-full overflow-hidden text-ellipsis whitespace-nowrap">${title.escape()}</h2>
+        <p class="text-sm text-gray-500 max-w-full overflow-hidden text-ellipsis whitespace-nowrap">${content.escape()}</p>
       </section>
     </article>
   `;
@@ -17,5 +24,24 @@ const PostPreview = ({ id, title = "제목", content = "내용", imgSrc = "" }) 
 
   return component;
 };
+
+const LoadingPostPreview = () => {
+  const componentString = `
+    <article class="flex rounded-md border bg-white border-gray-300 overflow-hidden cursor-pointer">
+      <section class="shrink-0 w-[100px] h-[100px] bg-gray-300 animate-pulse"></section>
+      <section class="flex flex-1 flex-col justify-between px-3 py-4 gap-2 overflow-hidden">
+        <h2 class="w-16 h-5 rounded-sm animate-pulse bg-gray-300"></h2>
+        <div class="flex flex-col gap-1">
+          <p class="w-full h-4 rounded-sm bg-gray-300 animate-pulse"></p>
+          <p class="w-[calc(100%-50px)] h-4 rounded-sm bg-gray-300 animate-pulse"></p>
+        </div>
+      </section>
+    </article>
+  `;
+
+  return parseElementFromString(componentString);
+};
+
+PostPreview.Loading = LoadingPostPreview;
 
 export default PostPreview;
