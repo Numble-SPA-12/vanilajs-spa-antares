6ca007e223d5f84cbba0629abe0af3ed9c9b2e0b
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-01-14 01:24:54 +0900

    홈 로딩 컴포넌트 구현

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -3,7 +3,7 @@ import parseElementFromString from "utils/parseElementFromString";
 const PostPreview = ({ id, title = "제목", content = "내용", imgSrc = "" }) => {
   const componentString = `
     <article id="newyear-post-${id}" data-post-id="${id}" class="flex rounded-md border bg-white border-gray-300 overflow-hidden cursor-pointer">
-      <section class="shrink-0 w-30 h-30">
+      <section class="shrink-0 w-[100px] h-[100px]">
         <img src="${imgSrc}" loading="lazy" />
       </section>
       <section class="flex flex-1 flex-col justify-between px-3 py-4 gap-2 overflow-hidden">
@@ -17,5 +17,24 @@ const PostPreview = ({ id, title = "제목", content = "내용", imgSrc = "" }) 
 
   return component;
 };
+
+const LoadingPostPreview = () => {
+  const componentString = `
+    <article class="flex rounded-md border bg-white border-gray-300 overflow-hidden cursor-pointer">
+      <section class="shrink-0 w-[100px] h-[100px] bg-gray-300 animate-pulse"></section>
+      <section class="flex flex-1 flex-col justify-between px-3 py-4 gap-2 overflow-hidden">
+        <h2 class="w-16 h-5 rounded-sm animate-pulse bg-gray-300"></h2>
+        <div class="flex flex-col gap-1">
+          <p class="w-full h-4 rounded-sm bg-gray-300 animate-pulse"></p>
+          <p class="w-[calc(100%-50px)] h-4 rounded-sm bg-gray-300 animate-pulse"></p>
+        </div>
+      </section>
+    </article>
+  `;
+
+  return parseElementFromString(componentString);
+};
+
+PostPreview.Loading = LoadingPostPreview;
 
 export default PostPreview;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -72,6 +72,7 @@ const Home = () => {
     <main class="flex-1 px-4 pt-4 pb-8 bg-gray-50">
       <button id="newyear-post__newpost-link" class="w-full py-3 rounded-lg bg-black text-white font-semibold text-lg mb-4">New Post</button>
       <ul id="newyear-post__list" class="flex flex-col gap-4">
+        ${PostPreview.Loading().outerHTML}
         ${data.map((post) => PostPreview(post).outerHTML).join("")}
       </ul>
     </main>
