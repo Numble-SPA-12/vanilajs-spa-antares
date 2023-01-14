51f19b9de5176ad1551bc279e518faa4c4d0d27e
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-01-14 12:40:00 +0900

    [Feat] Notfound 페이지 디자인 수정

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,14 +1,20 @@
 "use strict";
 
 const NotFound = () => {
-  const $main = document.createElement("main");
-  $main.classList.add(["flex", "flex-col", "justify-start"]);
-
-  $main.innerHTML = `
-    <h2>404</h2>  
+  const pageString = `
+    <div class="flex flex-col justify-start min-h-screen" > 
+      <app-header type="sub"></app-header>
+      <main class="flex-1 px-4 pt-4 pb-8 bg-gray-50 flex flex-col justify-center items-center gap-2">
+        <h1 class="text-4xl font-bold text-gray-900">404</h1>
+        <p class="text-xl text-gray-700">Page not found</p>
+        <a href="/" class="text-blue-500 mt-6">Go back to home</a>
+      </main>
+    </div> 
   `;
 
-  return $main;
+  const $page = document.createRange().createContextualFragment(pageString);
+
+  return $page;
 };
 
 export default NotFound;
