99f0ae49344c19609eda4370d9e563742b7bb072
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-01-18 01:29:31 +0900

    Not found 페이지 수정

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import Links from "common/constants/Links";
+
 const NotFound = () => {
   const pageString = `
     <div class="flex flex-col justify-start min-h-screen" > 
@@ -7,14 +9,18 @@ const NotFound = () => {
       <main class="flex-1 px-4 pt-4 pb-8 bg-gray-50 flex flex-col justify-center items-center gap-2">
         <h1 class="text-4xl font-bold text-gray-900">404</h1>
         <p class="text-xl text-gray-700">Page not found</p>
-        <a href="/" class="text-blue-500 mt-6">Go back to home</a>
+        <a href="${Links.Home}" class="text-blue-500 mt-6">Go back to home</a>
       </main>
     </div> 
   `;
 
-  const $page = document.createRange().createContextualFragment(pageString);
+  const render = () => {
+    const $app = document.querySelector("#app");
+    const $page = document.createRange().createContextualFragment(pageString);
+    $app.appendChild($page);
+  };
 
-  return $page;
+  render();
 };
 
 export default NotFound;
