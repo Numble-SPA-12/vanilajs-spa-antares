5885cb50f9c57cfbd0f54f48685410e9b15044e0
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2023-01-19 11:48:08 +0900

    [Feat] Home 링크 추가

--- src/common/components/Header.js
+++ src/common/components/Header.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import Links from "common/constants/Links";
 import router from "common/router";
 import parseElementFromString from "common/utils/parseElementFromString";
 
@@ -14,7 +15,7 @@ class Header extends HTMLElement {
 
   mainTemlate = `
     <header class="flex fixed z-20 bg-white top-0 items-center justify-start w-full gap-2 px-4 border-b border-gray-200 h-14 max-w-[640px]">
-        <h1 class="font-bold text-xl">HPNY 2023</h1>
+        <h1 role="link" tabindex="0" class="font-bold text-xl">HPNY 2023</h1>
         <div class="flex-1"></div>
     </header>
   `;
@@ -24,7 +25,7 @@ class Header extends HTMLElement {
       <button id="back-button" class="w-10 h-10">
         <i class="bi bi-chevron-left text-lg"></i>
       </button>
-      <h1 class="font-bold text-xl">HPNY 2023</h1>
+      <h1 role="link" tabindex="0" class="font-bold text-xl">HPNY 2023</h1>
       <div class="flex-1"></div>
     </header>
   `;
@@ -40,15 +41,28 @@ class Header extends HTMLElement {
     const $fragment = document.createDocumentFragment();
 
     switch (headerType) {
-      case "main":
-        $fragment.append(parseElementFromString(this.mainTemlate));
-        break;
-      case "sub":
-        const $header = parseElementFromString(this.subTemplate);
-        const $backButton = $header.querySelector("#back-button");
-        $backButton.addEventListener("click", this.backButtonClickHandler);
+      case "main": {
+        const $header = parseElementFromString(this.mainTemlate);
+
+        const $title = $header.querySelector("h1");
+        $title.addEventListener("click", this.titleClickHandler);
+        $title.addEventListener("keyup", this.titleEnterHandler);
+
         $fragment.append($header);
         break;
+      }
+      case "sub": {
+        const $header = parseElementFromString(this.subTemplate);
+
+        const $title = $header.querySelector("h1");
+        $title.addEventListener("click", this.titleClickHandler);
+        $title.addEventListener("keyup", this.titleEnterHandler);
+        const $backButton = $header.querySelector("#back-button");
+        $backButton.addEventListener("click", this.backButtonClickHandler);
+
+        $fragment.append($header);
+        break;
+      }
     }
     $fragment.append(parseElementFromString(this.placeholderTemplate));
 
@@ -69,6 +83,19 @@ class Header extends HTMLElement {
     const $target = e.target.closest("button");
     if ($target && $target.id === "back-button") {
       router.back();
+    }
+  }
+
+  titleClickHandler(e) {
+    const $target = e.target.closest("h1");
+    if ($target) {
+      router.push(Links.Home);
+    }
+  }
+
+  titleEnterHandler(e) {
+    if (e.key === "Enter") {
+      router.push(Links.Home);
     }
   }
 }
